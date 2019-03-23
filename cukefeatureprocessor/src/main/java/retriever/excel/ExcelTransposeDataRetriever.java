2c3e3ce683c99d666df474281741996c6d5598f3
Farid Guliyev <fg78nc@gmail.com>
2019-03-23 14:40:35 -0400

    Minor refactoring of parseRows methods

--- cukefeatureprocessor/src/main/java/retriever/excel/ExcelTransposeDataRetriever.java
+++ cukefeatureprocessor/src/main/java/retriever/excel/ExcelTransposeDataRetriever.java
@@ -1,14 +1,12 @@
 package retriever.excel;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.util.CellReference;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ExcelTransposeDataRetriever extends ExcelDataRetriever {
 
@@ -18,16 +16,14 @@ public class ExcelTransposeDataRetriever extends ExcelDataRetriever {
 	
 	@Override
 	public List<String> parseRows(Sheet sheet, CellReference[] crs) {
-		
-		List<String> data = new ArrayList<>();
-		Map<Short,String> coldata = new HashMap<>();
+
 		DataFormatter dataFormatter = new DataFormatter();
-		
-		Arrays.stream(crs).forEach( cr -> coldata.merge(cr.getCol(),
-				dataFormatter.formatCellValue(sheet.getRow(cr.getRow()).getCell(cr.getCol())),
-				(v1,v2) -> String.join(BOTH_SPACED_DELIMITER, v1, v2)));
-		
-		coldata.values().stream().forEach(v -> data.add(wrapLineSpace(v)));	
-		return data;
+
+		return Arrays
+				.stream(crs)
+				.collect(Collectors.toMap(CellReference::getCol,
+						cr -> dataFormatter.formatCellValue(sheet.getRow(cr.getRow()).getCell(cr.getCol())),
+						(v1, v2) -> String.join(BOTH_SPACED_DELIMITER, v1, v2)))
+				.values().stream().map(this::wrapLineSpace).collect(Collectors.toList());
 	}	
 }
